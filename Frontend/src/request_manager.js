9956748716f0cfcf192ed89417f1951613ffe431
evyatar <evbena@gmail.com>
2020-07-25 12:32:44 +0300

    fix ep

--- Frontend/src/request_manager.js
+++ Frontend/src/request_manager.js
@@ -1,16 +1,18 @@
 import axios from 'axios';
 
-let endPoint = ':8080';
+let endPoint = '';
 let url = '';
 let post_data = {};
 let headers = {}
+let baseURL = 'http://localhost:8080';
+baseURL = 'http://139.162.208.159:8080'
 headers = {'Content-Type': "application/json"};
 
 function make_server_request(server_call) {
 
     switch (server_call) {
       case "login":
-        url = endPoint + '/login';
+        url = `${endPoint}/login`;
         return get_function(url);
         break;
      
@@ -18,12 +20,13 @@ function make_server_request(server_call) {
 }
 
 function get_function(url) {
-  console.log('testttmp')
+  console.log(url)
 
 let ax =  axios({
       method: 'get',
       headers: headers,
       url: url,
+      baseURL: baseURL
     })
 
   return ax;
